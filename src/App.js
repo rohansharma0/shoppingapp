04587e0d52deb44aba32fa9bce808d7cb8fd7b79
Rohan Sharma <iamrohan29@gmail.com>
2021-04-21 02:22:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { toast, ToastContainer } from "react-toastify";
+import BuyPage from "./Components/BuyPage";
+import { Container, Row, Col } from "reactstrap";
+import Cart from "./Components/Cart";
 
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addInCart = (item) => {
+    const isAreadyAdded = cartItem.findIndex(function (array) {
+      return array.id === item.id;
+    });
+    if (isAreadyAdded !== -1) {
+      toast("Aread added in cart", {
+        type: "error",
+      });
+      return;
+    }
+
+    setCartItem([...cartItem, item]);
+  };
+
+  const buyNow = () => {
+    setCartItem([]);
+    toast("Order Done!", {
+      type: "success",
+    });
+  };
+
+  const removeInCart = (item) => {
+    setCartItem(cartItem.filter((singleItem) => singleItem.id !== item.id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <ToastContainer />
+      <Row>
+        <Col md={8}>
+          <BuyPage addInCart={addInCart} />
+        </Col>
+        <Col md={4}>
+          <Cart cartItem={cartItem} removeItem={removeInCart} buyNow={buyNow} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
